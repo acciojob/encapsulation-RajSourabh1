e27536baa516e7ea4662d377358db941f13f6dff
Sourabh <createownpath111@gmail.com>
2022-12-24 13:17:21 +0530

    codes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+import java.net.SocketTimeoutException;
+import java.sql.SQLOutput;
+
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj = new RWOnly();
+      // 'name' has private access in 'com.driver.RWOnly'
+      // obj.name = "Sourabh";
+      obj.setName("Sourabh");
+      System.out.println(obj.getName());
+  }
 }
